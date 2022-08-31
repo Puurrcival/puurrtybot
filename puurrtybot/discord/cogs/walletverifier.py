4da66d6d18ceaeae1f360ec9f224a6273570eb55
puurrcival <puurrcival@gmail.com>
2022-08-31 06:23:32 +0200

    minor fixes

--- puurrtybot/discord/cogs/walletverifier.py
+++ puurrtybot/discord/cogs/walletverifier.py
@@ -40,13 +40,14 @@ class WalletVerifier(commands.Cog):
             content = f"""{interaction.user.mention}, this address has been verified already: {address.address}"""
         else:
             amount = str(random.choice(list(range(2_000_000, 3_000_000+1))))
-            content = f"""**Verify a new address** \n\n⌛ Please send **{amount}₳** to your own address at **{address.address}** within the next 60 minutes.\n\n⚠ Make sure to send from (and to) the wallet that owns this address. In case of error your fees will not be reimbursed by the operator of this Discord server.\n\n Will check every 3 minutes for transaction."""
+            content = f"""**Verify a new address** \n\n⌛ Please send **{amount}₳** to your own address at **{address.address}** within the next 60 minutes.\n\n⚠ Make sure to send from (and to) the wallet that owns this address. In case of error your fees will not be reimbursed by the operator of this Discord server.\n\n Will check every 3 minutes for transaction.\n\nNext check: <t:{int(datetime.datetime.now().timestamp())+time_window}:R>."""
         await interaction.response.send_message(content, ephemeral = True)
 
         if amount:
             time_window = 3*60
-            address_list = blockfrostio.get_address_list_by_stake_address(address.stake_address) if address.stake_address else []
+            await asyncio.sleep(time_window)
             message = await ctx.send(f"""Next check: <t:{int(datetime.datetime.now().timestamp())+time_window}:R>.""", ephemeral=True)
+            address_list = blockfrostio.get_address_list_by_stake_address(address.stake_address) if address.stake_address else []
             for _ in range(20):
                 if await verify_transaction(address.address, time_window, amount, address_list) and not interaction.is_expired():
                     content = f"""<@{address.user_id}>, transaction found, your address is now verified: {address.address}"""
@@ -55,7 +56,7 @@ class WalletVerifier(commands.Cog):
                     await update_role_all_by_user(dq.fetch_row(User(address.user_id)))     
                     break
                 await asyncio.sleep(time_window)
-                if not interaction.is_expired(): await message.edit(f"""Next check: <t:{int(datetime.datetime.now().timestamp())+time_window}:R>.""")
+                if not interaction.is_expired(): await message.edit(content = f"""Next check: <t:{int(datetime.datetime.now().timestamp())+time_window}:R>.""")
     
 
 async def setup(bot: commands.Bot) -> None:
